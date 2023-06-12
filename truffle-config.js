bd313d6925344bd19dcf9449feaf8b5f196a6d79
Dhiraj Bari <98685837+dhi-raj07@users.noreply.github.com>
2023-06-12 13:23:49 +0530

    networks

--- truffle-config.js
+++ truffle-config.js
@@ -2,30 +2,9 @@ const path = require("path");
 require("dotenv").config();
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-const mnemonic = process.env.MNEMONIC;
-const alchemyKey = process.env.ALCHEMY;
-
 module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    mumbai: {
-      provider: function () {
-        return new HDWalletProvider(
-          mnemonic,
-          `https://polygon-mumbai.g.alchemy.com/v2/${alchemyKey}`
-        );
-      },
-      network_id: 80001,
-    },
-    goerli: {
-      provider: function () {
-        return new HDWalletProvider(
-          mnemonic,
-          `https://eth-goerli.g.alchemy.com/v2/${alchemyKey}`
-        );
-      },
-      network_id: 5,
-    },
     ganache: {
       host: "127.0.0.1",
       port: 8545, 
